0e21e4b5474e398424ff712b5707f97d31ea7ab5
Damir Samatov <damir_samatov@epam.com>
2025-10-30 23:49:14 +0500

    task-6

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,8 @@
-export const FRONTEND_URL = "https://d1q8poe4mvoaix.cloudfront.net"
-export const PRODUCT_TABLE_NAME = "Product"
-export const STOCK_TABLE_NAME = "Stock"
-export const UPLOADED_BUCKET_FOLDER = "uploaded/"
-export const PARSED_BUCKET_FOLDER = "parsed/"
+export const FRONTEND_URL = "https://d1q8poe4mvoaix.cloudfront.net";
+export const PRODUCT_TABLE_NAME = "Product";
+export const STOCK_TABLE_NAME = "Stock";
+export const UPLOADED_BUCKET_FOLDER = "uploaded/";
+export const PARSED_BUCKET_FOLDER = "parsed/";
+export const ENV_MISSING_ERROR = "Environment variables missing!";
+export const INTERNAL_SERVER_ERROR = "Internal server error!";
+export const SNS_CREATE_PRODUCTS_TOPIC_SUBSCRIBERS = ["damir_samatov@epam.com"];
